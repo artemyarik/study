cc33de1648b426345406acaa4beaf571e8d4ceb9
Artem <artem.yasnov1@live.com>
2022-08-04 12:13:44 +0300

    destruct object

---
+++ Деструктуризация/destruct.js
@@ -0,0 +1,11 @@
+const bears = {
+    name: "Alesha",
+    fat: "212",
+    colors:  {
+        border: "black",
+        bg: "red"
+    }
+};
+
+const {border, bg} = bears.colors;
+console.log(border, bg);
